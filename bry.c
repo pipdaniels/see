a95f2eb943a31e38318992756872c787c6e1716e
Seun <deluxehay1@gmail.com>
2025-12-19 15:43:07 +0100

    nn

--- bry.c
+++ bry.c
@@ -2,6 +2,18 @@
 #include <stdbool.h>
 
 int main (){
-  
+  bool isLoggedIn = true;
+  bool isAdmin = false;
+  int clearanceLevel = 2;
 
+  if ( isLoggedIn && (isAdmin || clearanceLevel <= 2)){
+    printf("Access Granted\n");
+
+  } else {
+    printf("Acess Denied\n");
+  }
+
+  return 0;
 }
+
+
